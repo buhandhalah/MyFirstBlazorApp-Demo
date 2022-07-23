9d37a7ba08a4a347d076c83c3d9b647e82f3a44e
Mohammed Ameer <ameer240995@gmail.com>
2022-07-23 14:42:17 +0400

    Add project files.

---
+++ Models/Contact.cs
@@ -0,0 +1,9 @@
+﻿namespace MyFirstBalzorServerApp.Models
+{
+    public class Contact
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ Pages/Index.razor.cs
@@ -0,0 +1,45 @@
+﻿using MyFirstBalzorServerApp.Models;
+
+namespace MyFirstBalzorServerApp.Pages
+{
+    public partial class Index
+    {
+        private List<Contact> contacts;
+        private Dictionary<string, object> MyTextBoxAttributes = new Dictionary<string, object>
+    {
+      {"placeholder","FirstName"},
+      {"disabled","disabled"}
+    };
+
+        private void DeleteContact(Contact contact)
+        {
+            contacts.Remove(contact);
+        }
+
+        protected async override Task OnInitializedAsync()
+        {
+            await Task.Delay(5000);
+            //contacts = new List<Contact>();
+            contacts = new List<Contact>
+        {
+            new Contact
+            {
+                FirstName = "John",
+                LastName = "Th",
+                Email = "Johnth@gmail.com",
+            }, new Contact
+            {
+                FirstName = "Don",
+                LastName = "Th",
+                Email = "Donth@gmail.com",
+            }, new Contact
+            {
+                FirstName = "Bon",
+                LastName = "Th",
+                Email = "Bonth@gmail.com",
+            }
+        };
+            base.OnInitializedAsync();
+        }
+    }
+}
